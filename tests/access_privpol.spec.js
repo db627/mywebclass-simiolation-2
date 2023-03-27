f0d3cad476ad280cf9513e4835669e72cf83b084
PaulWojtowicz <115052715+PaulWojtowicz@users.noreply.github.com>
2023-03-27 04:39:39 -0400

    Accessing Privacy Policy
    This test was created to ensure that access to the privacy policy can be gained through the Cookies pop-up

--- tests/access_privpol.spec.js
+++ tests/access_privpol.spec.js
@@ -4,6 +4,6 @@ test('test', async ({ page }) => {
   await page.goto('https://db627.github.io/mywebclass-simiolation-2/');
   const page1Promise = page.waitForEvent('popup');
   await page.locator('#privacyModal').getByRole('link', { name: 'Privacy Policy' }).click();
-  const page1 = await page1Promise;
+  const page1 = awaiit page1Promise;
   await page1.getByRole('button', { name: 'Agree', exact: true }).click();
 });
