12a824277964e2c817688ba6b8520b3ad22af952
PaulWojtowicz <115052715+PaulWojtowicz@users.noreply.github.com>
2023-03-27 04:57:02 -0400

    Delete typeemailandsub.spec.js

--- typeemailandsub.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.getByPlaceholder('Email address').click();
-  await page.getByPlaceholder('Email address').fill('pw8@njit.edu');
-  await page.getByRole('button', { name: 'Subscribe' }).click();
-});
