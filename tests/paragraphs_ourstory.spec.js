f5c7ccafe0a920d1db7dc14c84c70b1fd6691601
PaulWojtowicz <115052715+PaulWojtowicz@users.noreply.github.com>
2023-03-27 05:04:42 -0400

    Create paragraphs_ourstory.spec.js
    This test ensures that the paragraphs on the our story page all appear

---
+++ tests/paragraphs_ourstory.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.getByText('Our mission is to create a community of people committed to helping students of ').click();
+  await page.getByText('People: We value the people in our community and believe that every student and ').click();
+  await page.getByText('Learning: We believe that learning is a lifelong process and that everyone has s').click();
+  await page.getByText('Efficiency: We recognize that time is a valuable resource, and we strive to make').click();
+  await page.getByText('Dependability: We understand the importance of being reliable and consistent. We').click();
+  await page.getByText('Each of these values supports our mission by creating a welcoming and supportive').click();
+  await page.getByText('Mywebclass.org was founded in 2023 by Keith Williams, a University Lecturer at N').click();
+  await page.getByText('Thank you for your interest in mywebclass.org. Together, we can create a communi').click();
+});
