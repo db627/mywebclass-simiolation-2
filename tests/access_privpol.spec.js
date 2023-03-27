98be2144ca7f4db6bb7298cbb6b7161b8acaedf1
PaulWojtowicz <115052715+PaulWojtowicz@users.noreply.github.com>
2023-03-27 03:03:05 -0400

    Create access_privpol.spec.js

---
+++ tests/access_privpol.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://db627.github.io/mywebclass-simiolation-2/');
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('#privacyModal').getByRole('link', { name: 'Privacy Policy' }).click();
+  const page1 = await page1Promise;
+  await page1.getByRole('button', { name: 'Agree', exact: true }).click();
+});
