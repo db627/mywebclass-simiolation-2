140ff31ae35f5492d925a9e9f270c2eae9c6cfde
db627 <db627@njit.edu>
2023-03-27 21:36:02 -0400

    fix privacy nav

--- tests/socialmedia_home.spec.js
+++ tests/socialmedia_home.spec.js
@@ -1,8 +1,9 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://db627.github.io/mywebclass-simulation-2/')
-  await page.getByRole('link', { name: '' }).click();
-  await page.getByRole('link', { name: '' }).click();
-  await page.getByRole('link', { name: '' }).click();
+  await page.getByRole('link', { name: 'Linkedin' }).click();
+  await page.getByRole('link', { name: 'Twitter' }).click();
+  await page.getByRole('link', { name: 'Facebook' }).click();
 });
